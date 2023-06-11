1fe3129d0bf9286e1bb4d55ca5188fdb63f48a19
inamali8093 <124720867+inamali8093@users.noreply.github.com>
2023-06-11 06:05:15 +0000

    variables explanation

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=2359
+var accountName="adam"
+let accountEmail="adam@123"
+let accountPassword;
+// this is used to print in table form
+console.table([accountId,accountName,accountEmail,accountPassword])
