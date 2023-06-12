3e77f185b90b6ed4bdbca31ee473c329f83b8f16
inamali8093 <124720867+inamali8093@users.noreply.github.com>
2023-06-12 07:06:14 +0000

    datatypes explation

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as new version
+
+// alert("hello")//we are using node js not browser
+
+//types of datatypes in js
+// Number=>2 to the power 53
+// BigInt=>
+// String=>""
+// Boolean=>true/false
+// null=>standalone value
+// undefined=>
+// symbol=>unique
+
+
+//object
+//arrays
+
+console.log(typeof "33abc");
+console.log(typeof null);// object
